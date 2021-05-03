be4e0a0a94a75cb2c75a3243c45d9ad14529fca5
Maykon Lacerda <maykonalvslacerda@gmail.com>
2021-05-03 20:07:46 -0300

    create table Productions by Worker

---
+++ src/database/migrations/1620081810049-CreateProduction.ts
@@ -0,0 +1,60 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+
+export class CreateProduction1620081810049 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.createTable(
+            new Table({
+                name: "productions",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "worker_id",
+                        type: "uuid",
+                    },
+                    {
+                        name: "amount",
+                        type: "integer",
+                    },
+                    {
+                        name: "task",
+                        type: "varchar"
+                    },
+                    {
+                        name: "day",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
+
+        await queryRunner.createForeignKey(
+            "productions",
+         new TableForeignKey({
+             name: "FKProductionWorker",
+             referencedTableName: "workers",
+             referencedColumnNames: ["id"],
+             columnNames: ["worker_id"],
+             onDelete: "SET NULL",
+             onUpdate: "CASCADE"
+         })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey('productions', 'FKProductionWorker');
+        await queryRunner.dropTable('productions');
+    }
+
+}
---
+++ src/entities/Production.ts
@@ -0,0 +1,35 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, UpdateDateColumn } from "typeorm"
+
+import { v4 as uuidv4} from "uuid"
+import { Worker } from "./Worker"
+
+@Entity("productions")
+class Production {
+
+    @PrimaryColumn()
+    id: string
+
+    @JoinColumn({ name: "worker_id" })
+    @ManyToOne(() => Worker)
+    worker: Worker
+
+    @Column()
+    amount: string
+
+    @Column()
+    task: string
+
+    @CreateDateColumn()
+    day: Date
+
+    @UpdateDateColumn()
+    updated_at: Date
+
+    constructor() {
+        if(!this.id) {
+            this.id = uuidv4()
+        }
+    }
+}
+
+export { Production }
